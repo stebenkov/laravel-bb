82f095490e95e5d82a42d6b268c5c309b6ce05dc
Mikhail Stebenkov <stebenkovmikhail@gmail.com>
2026-02-26 00:25:23 +0400

    Hello World controller

---
+++ app/Http/Controllers/HelloWorldController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+class HelloWorldController extends Controller
+{
+    public function index() {
+        return "Aloha!";
+    }
+}
